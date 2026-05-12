a8990461ba1d8f0673b203d53ec628f0e8a97756
papjamzzz <127992026+papjamzzz@users.noreply.github.com>
2026-05-11 22:46:52 -0400

    Switch to Flask + gunicorn

--- server.py
+++
@@ -1,7 +0,0 @@
-import os
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-
-port = int(os.environ.get("PORT", 8080))
-server = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
-print(f"Serving on port {port}")
-server.serve_forever()
